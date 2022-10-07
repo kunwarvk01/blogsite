e39789ee918027fcd606cbcbb4569f0e40c52866
kunwarvk01 <kunwarvk01@gmail.com>
2022-10-07 19:40:54 +0530

    ejs added

--- app.js
+++ app.js
@@ -16,8 +16,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
+    res.render("home", { startingContent: homeStartingContent });
+});
 
-})
+app.get("/about", function(req, res) {
+    res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
 
 
 
